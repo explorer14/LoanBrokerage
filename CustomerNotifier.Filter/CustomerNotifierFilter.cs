6650fc5f7c3e1555b09ad127444c941ddf262df0
explorer14 <amanag1983@gmail.com>
2018-07-08 19:49:32 +0200

    Addressed BCH feedback 1

--- CustomerNotifier.Filter/CustomerNotifierFilter.cs
+++ CustomerNotifier.Filter/CustomerNotifierFilter.cs
@@ -11,36 +11,28 @@ namespace CustomerNotifier.Filter
         private readonly ICustomerRepository customerRepository;
         private readonly ILogger logger;
 
-        public CustomerNotifierFilter(
-            INotificationService notificationService,
-            ICustomerRepository customerRepository,
-            ILogger logger)
+        public CustomerNotifierFilter(INotificationService notificationService, ICustomerRepository customerRepository, ILogger logger)
         {
             this.notificationService = notificationService;
             this.customerRepository = customerRepository;
             this.logger = logger;
         }
 
-        public async Task NotifyCustomer(
-            CustomerLoanQuote customerLoanQuote)
+        public async Task NotifyCustomer(CustomerLoanQuote customerLoanQuote)
         {
             if (customerLoanQuote == null)
-                throw new ArgumentNullException(
-                    $"{nameof(customerLoanQuote)} is null!");
+            {
+                throw new ArgumentNullException($"{nameof(customerLoanQuote)} is null!");
+            }
 
             if (customerLoanQuote?.Quotes == null)
-                throw new ArgumentNullException(
-                    $"{nameof(CustomerLoanQuote.Quotes)} is null!");
+            {
+                throw new ArgumentNullException($"{nameof(CustomerLoanQuote.Quotes)} is null!");
+            }
 
             logger.Information("Retrieving customer information from the database...");
-            var customerInfo = await customerRepository
-                .CustomerByBSN(
-                customerLoanQuote.BSN);
-            await notificationService
-                .Notify(
-                customerInfo.Email,
-                JsonConvert.SerializeObject(
-                    customerLoanQuote.Quotes));
+            var customerInfo = await customerRepository.CustomerByBSN(customerLoanQuote.BSN);
+            await notificationService.Notify(customerInfo.Email, JsonConvert.SerializeObject(customerLoanQuote.Quotes));
         }
     }
 }
