201433241dc577e134188872c30c7d7b6c0068f8
explorer14 <amanag1983@gmail.com>
2018-07-08 20:51:22 +0200

    Addressed BCH feedback 2

--- CreditChecker.Filter/CreditCheckFilter.cs
+++ CreditChecker.Filter/CreditCheckFilter.cs
@@ -13,61 +13,33 @@ namespace CreditChecker.Filter
         private readonly IHttpMessageHandlerFactory httpMessageHandlerFactory;
         private readonly ILogger logger;
 
-        public CreditCheckFilter(
-            IHttpMessageHandlerFactory httpMessageHandlerFactory,
-            ILogger logger)
+        public CreditCheckFilter(IHttpMessageHandlerFactory httpMessageHandlerFactory, ILogger logger)
         {
             this.httpMessageHandlerFactory = httpMessageHandlerFactory;
             this.logger = logger;
         }
 
-        public async Task<EnrichedLoanRequest> PerformCreditCheck(
-            LoanRequest loanRequest)
+        public async Task<EnrichedLoanRequest> PerformCreditCheck(LoanRequest loanRequest)
         {
             if (loanRequest.RequestedLoanAmount == 0 || string.IsNullOrWhiteSpace(loanRequest.CitizenServiceNumber))
+            {
                 throw new ArgumentException("Requested loan amount must be > 0 and BSN must be provided");
+            }
 
-            EnrichedLoanRequest enrichedLoanRequest =
-                default(EnrichedLoanRequest);
+            EnrichedLoanRequest enrichedLoanRequest = default(EnrichedLoanRequest);
 
-            using (var httpClient = new HttpClient(
-                httpMessageHandlerFactory.Create(),
-                true))
+            using (var httpClient = new HttpClient(httpMessageHandlerFactory.Create(), true))
             {
                 httpClient.BaseAddress = httpMessageHandlerFactory.BaseUri;
 
-                // TODO: this is a very chatty approach to credit check.
-                // Perhaps a single call with all the loan requests in
-                // an array is a better solution as long as the response
-                // comes back within the HTTP timeout of 2 minutes.
-                // Alternatively, batching can be used to send "x" requests
-                // per call, will be helpful if the number of loan requests
-                // is really high. At the other end, having a queue behind the HTTP API
-                // will also help with load levelling.
-                var stringContent = new StringContent(
-                    JsonConvert.SerializeObject(loanRequest),
-                    Encoding.UTF8,
-                    "application/json");
-                httpClient.DefaultRequestHeaders.Add(
-                    "Request-Id",
-                    loanRequest.CitizenServiceNumber);
+                var stringContent = new StringContent(JsonConvert.SerializeObject(loanRequest), Encoding.UTF8, "application/json");
+                httpClient.DefaultRequestHeaders.Add("Request-Id", loanRequest.CitizenServiceNumber);
                 logger.Information("Contacting credit check agency...");
-                var response = await httpClient.PutAsync("api/creditcheck",
-                    stringContent);
+                var response = await httpClient.PutAsync("api/creditcheck", stringContent);
 
                 if (response.IsSuccessStatusCode)
                 {
-                    // TODO: at the moment, the API call returns the result
-                    // of the credit check. In the future it will simply return
-                    // a 201 Accepted i.e. queued for processing so our application
-                    // doesn't have to sit and wait for the response. We could
-                    // expose another queue fronted by an API to which the Credit
-                    // Agency can write the results of credit check. We will be listening
-                    // on that queue and can start processing as soon as there are messages
-                    // on that queue.
-                    var content = await response
-                        .Content
-                        .ReadAsStringAsync();
+                    var content = await response.Content.ReadAsStringAsync();
 
                     dynamic obj = JsonConvert.DeserializeObject(content);
 
