0cf3148f7b96bfb6d8b658fc722fb8a0242ad7bb
explorer14 <amanag1983@gmail.com>
2018-07-08 20:59:05 +0200

    Addressed BCH feedback 5

--- LoanRequestSender/Functions.cs
+++ LoanRequestSender/Functions.cs
@@ -14,39 +14,28 @@ namespace LoanRequestSender
         private readonly IPipe<LoanQuoteResponse> pipe;
         private readonly ILogger logger;
 
-        public Functions(ILoanRequestSenderFilter loanRequestSenderFilter,
-            IPipe<LoanQuoteResponse> pipe,
-            ILogger logger)
+        public Functions(ILoanRequestSenderFilter loanRequestSenderFilter, IPipe<LoanQuoteResponse> pipe, ILogger logger)
         {
             this.loanRequestSenderFilter = loanRequestSenderFilter;
             this.pipe = pipe;
             this.logger = logger;
         }
 
-        public async Task ProcessQueueMessage(
-            [QueueTrigger("credit-checked-loan-requests")] string message,
-            TextWriter log)
+        public async Task ProcessQueueMessage([QueueTrigger("credit-checked-loan-requests")] string message, TextWriter log)
         {
             logger.Information("Retrieving credit checked loan request...");
             dynamic creditCheckedLoanRequest = JsonConvert.DeserializeObject(message);
             var loanQuoteRequest = new LoanQuoteRequest
             {
-                BSN = creditCheckedLoanRequest
-                .OriginalLoanRequest
-                .CitizenServiceNumber,
+                BSN = creditCheckedLoanRequest.OriginalLoanRequest.CitizenServiceNumber,
 
                 CreditRating = $"{creditCheckedLoanRequest.CreditCheckReport.CreditScore} " +
                 $"({creditCheckedLoanRequest.CreditCheckReport.CreditRating})",
 
-                LoanAmount = creditCheckedLoanRequest
-                .OriginalLoanRequest
-                .RequestedLoanAmount
+                LoanAmount = creditCheckedLoanRequest.OriginalLoanRequest.RequestedLoanAmount
             };
-            logger.Information(
-                "Now sending the loan quote {@request} to all the registered banks...",
-                loanQuoteRequest);
-            var quoteResponse = await loanRequestSenderFilter
-                .GetLoanQuotesFromRegisteredBanks(loanQuoteRequest);
+            logger.Information("Now sending the loan quote {@request} to all the registered banks...", loanQuoteRequest);
+            var quoteResponse = await loanRequestSenderFilter.GetLoanQuotesFromRegisteredBanks(loanQuoteRequest);
             await pipe.Write(quoteResponse);
             logger.Information("Loan Request Sender Done!");
         }
