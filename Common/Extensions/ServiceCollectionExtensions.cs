5431d4173e3d8a2f293e08c41c146087fac1cbe8
explorer14 <amanag1983@gmail.com>
2018-07-08 20:57:43 +0200

    Addressed BCH feedback 4

--- Common/Extensions/ServiceCollectionExtensions.cs
+++ Common/Extensions/ServiceCollectionExtensions.cs
@@ -10,31 +10,11 @@ namespace Common.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static ServiceCollection AddSplunkLogging(
-            this ServiceCollection services,
-            SplunkOptions splunkOptions)
+        public static ServiceCollection AddSplunkLogging(this ServiceCollection services, SplunkOptions splunkOptions)
         {
             var handler = new HttpClientHandler();
-            handler.ServerCertificateCustomValidationCallback +=
-                     (HttpRequestMessage message,
-                     X509Certificate2 cert,
-                     X509Chain chain,
-                     SslPolicyErrors policyErrors) => true;
-
-            services.AddSingleton<ILogger>(_ =>
-                new LoggerConfiguration()
-                .WriteTo
-                .Console()
-                .WriteTo
-                .EventCollector(
-                    splunkOptions.SplunkHost,
-                    splunkOptions.Token,
-                    new SplunkJsonFormatter(true, null),
-                    "services/collector",
-                    LogEventLevel.Verbose,
-                    "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}",
-                    2, 100, handler)
-                .CreateLogger());
+            handler.ServerCertificateCustomValidationCallback += (HttpRequestMessage message, X509Certificate2 cert, X509Chain chain, SslPolicyErrors policyErrors) => true;
+            services.AddSingleton<ILogger>(_ => new LoggerConfiguration().WriteTo.Console().WriteTo.EventCollector(splunkOptions.SplunkHost, splunkOptions.Token, new SplunkJsonFormatter(true, null), "services/collector", LogEventLevel.Verbose, "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}", 2, 100, handler).CreateLogger());
 
             return services;
         }
